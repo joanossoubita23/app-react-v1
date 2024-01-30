fb1bdda02ee3bde8700a1970685eaec9872fc607
joanossoubita23 <joanossoubita@yahoo.fr>
2024-01-29 22:05:51 -0500

    firstApp

---
+++ src/App.js
@@ -0,0 +1,66 @@
+
+import './App.css';
+import './App2';
+
+
+function App() {
+  return (
+   <section>
+
+    <h1>Hello App</h1>
+    <img 
+    className='Lion'
+    src="https://sp.yimg.com/ib/th?id=OPHS.GEXAjOurYx%2bTvg474C474&o=5&pid=21.1&w=160&h=105" 
+    width={500}
+    height={400}
+    alt=" Cat1"/>
+    <img src="https://sp.yimg.com/ib/th?id=OPHS.GEXAjOurYx%2bTvg474C474&o=5&pid=21.1&w=160&h=105" 
+    width={500}
+    height={400}alt='Cat2'/>
+    <img src="https://sp.yimg.com/ib/th?id=OPHS.GEXAjOurYx%2bTvg474C474&o=5&pid=21.1&w=160&h=105" 
+    width={500}
+    height={400}alt='Cat3'/>
+    
+   </section>
+   
+  );
+}
+
+
+// function App2() {
+//   return (
+//    <section>
+
+//     <h1>Hello App</h1>
+//     <img src="https://sp.yimg.com/ib/th?id=OPHS.GEXAjOurYx%2bTvg474C474&o=5&pid=21.1&w=160&h=105" alt=" Cat1"/>
+//     <img src="https://sp.yimg.com/ib/th?id=OPHS.GEXAjOurYx%2bTvg474C474&o=5&pid=21.1&w=160&h=105" alt='Cat2'/>
+//     <img src="https://sp.yimg.com/ib/th?id=OPHS.GEXAjOurYx%2bTvg474C474&o=5&pid=21.1&w=160&h=105" alt='Cat3'/>
+    
+//    </section>
+   
+//   );
+// }
+ 
+// export default function Profile(){
+//   return(
+
+//     <img
+//     src='/Users/joanmelckioroyameossoubita/code/react/firstapp/src/source/Screenshot 2024-01-24 at 11.50.59 PM.png'
+//     alt='docuemnt'
+//     />
+//   );
+// }
+// function Gallery(){
+//   return(
+//     <section>
+//     <h1>New Profile</h1>
+//     <Profile/>
+//     <Profile/>
+//     <Profile/>
+//     </section>
+//   );
+// }
+
+export default App;
+
+
---
+++ src/App2.js
@@ -0,0 +1,14 @@
+function App2() {
+    return (
+     <section>
+  
+      <h1>Hello App</h1>
+      <img src="https://sp.yimg.com/ib/th?id=OPHS.GEXAjOurYx%2bTvg474C474&o=5&pid=21.1&w=160&h=105" alt=" Cat1"/>
+      <img src="https://sp.yimg.com/ib/th?id=OPHS.GEXAjOurYx%2bTvg474C474&o=5&pid=21.1&w=160&h=105" alt='Cat2'/>
+      <img src="https://sp.yimg.com/ib/th?id=OPHS.GEXAjOurYx%2bTvg474C474&o=5&pid=21.1&w=160&h=105" alt='Cat3'/>
+      
+     </section>
+     
+    );
+  }
+  export default App2;
